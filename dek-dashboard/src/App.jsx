a96f8affbf779383deaca9fbc73763b63af4a660
krewsona <ajkrewson@gmail.com>
2025-06-01 15:00:39 -0500

    error fix

--- dek-dashboard/src/App.jsx
+++ dek-dashboard/src/App.jsx
@@ -9,7 +9,7 @@ import About from './pages/About';
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Login />} />
@@ -19,7 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
-    </HashRouter>
+    </>
   );
 }
 
